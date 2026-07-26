1c222a6d01cd6590a0d69c37e553a8dbb0b58f98
PriyaRanjan018 <priyaranjanpradhan005@gmail.com>
2026-07-26 20:03:54 +0530

    hide sensetive data

--- check_schema_2.js
+++
@@ -1,8 +0,0 @@
-async function run() {
-  const { createClient } = require("@supabase/supabase-js");
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-  const { data, error } = await supabase.from('entries').select('*').limit(1);
-  if (error) console.log(error);
-  else console.log(data);
-}
-run();
